e250ef7ffc195eebca204d02d588e2d3ac38cc30
cv <yrmm_cv@163.com>
2021-12-10 12:29:29 +0800

    Target 2 Print a page table

--- kernel/vm.c
+++ kernel/vm.c
@@ -432,3 +432,37 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+
+void vmprint_h(pagetable_t pagetable, int level)
+{
+  if(level > 2)
+  {
+    return;
+  }
+  for(int i = 0; i < 512; i++)
+  {
+    pte_t pte = pagetable[i];
+    if(pte & PTE_V)
+    {
+      uint64 pa = PTE2PA(pte);
+      for(int j = 0; j <= level; j++)
+      {
+        printf("..");
+        if(j != level)
+        {
+          printf(" ");
+        }
+      }
+      printf("%d: pte %p pa %p\n", i, pte, pa);
+      vmprint_h((pagetable_t)pa, level+1);
+    }
+  }
+}
+
+void vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable);
+  vmprint_h(pagetable, 0);
+}
+
