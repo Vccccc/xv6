a9ccc0712ae16232d7c7f46cc24103491f6935fe
cv <yrmm_cv@163.com>
2021-12-15 17:45:42 +0800

    target 1 System call tracing

--- kernel/proc.c
+++ kernel/proc.c
@@ -119,6 +119,7 @@ allocproc(void)
 found:
   p->pid = allocpid();
   p->state = USED;
+  p->tracemask = 0;
 
   // Allocate a trapframe page.
   if((p->trapframe = (struct trapframe *)kalloc()) == 0){
@@ -288,6 +289,7 @@ fork(void)
     return -1;
   }
   np->sz = p->sz;
+  np->tracemask = p->tracemask;
 
   // copy saved user registers.
   *(np->trapframe) = *(p->trapframe);
@@ -653,4 +655,11 @@ procdump(void)
     printf("%d %s %s", p->pid, state, p->name);
     printf("\n");
   }
+}
+
+int trace(int mask)
+{
+  struct proc* p = myproc();
+  p->tracemask = mask;
+  return 0;
 }
