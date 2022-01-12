327893fc4e194976faf55aa6bae38d225d50327a
cv <yrmm_cv@163.com>
2022-01-12 15:27:47 +0800

    target 3 Barrier. TODO: remove curround

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -30,7 +30,20 @@ barrier()
   // Block until all threads have called barrier() and
   // then increment bstate.round.
   //
-  
+  pthread_mutex_lock(&bstate.barrier_mutex);
+  bstate.nthread++;
+  int curround = bstate.round;
+  while(bstate.nthread != nthread && bstate.round == curround)
+  {
+    pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+  }
+  if(curround == bstate.round)
+  {
+    bstate.nthread = 0;
+    bstate.round++;
+  }
+  pthread_cond_broadcast(&bstate.barrier_cond);  
+  pthread_mutex_unlock(&bstate.barrier_mutex);
 }
 
 static void *
